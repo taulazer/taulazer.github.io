decf3fd98a57d0164a36cdca9ea3434e6b25530b
Coppertine <37494321+Coppertine@users.noreply.github.com>
2021-11-06 23:07:34 +1000

    Replace WIP with icons instead They could turn away new users of Tau..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './Logo.svg';
 import './App.css';
+import {FaDiscord} from 'react-icons/fa';
+import {FaGithub} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
 
 function App() {
   return (
@@ -7,12 +10,21 @@ function App() {
       <header className="App-header">
           <img src={logo} alt="Tau Logo"/>
           <p>Sweeping beats with a scythe.</p>
-          <a className="btn-download" href="https://github.com/taulazer/tau/releases/download/2021.1103.0/osu.Game.Rulesets.Tau.dll">
+          <a className="btn-download" href="https://github.com/taulazer/tau/releases/">
               <span>Download</span></a>
       </header>
         <footer className="App-footer">
-            <p className="text-bold">This website is still being worked on!</p>
-            <p>However, for now you can visit the <a href="https://rulesets.info/rulesets/tau">ruleset page</a> for more details on this ruleset!</p>
+        {/*    <p className="text-bold">This website is still being worked on!</p>*/}
+        {/*    <p>However, for now you can visit the <a href="https://github.com/taulazer/tau#readme">ruleset page</a> for more details on this ruleset!</p>*/}
+            <div className="social-links">
+                {/*  Discord  */}
+                <a href="https://discord.gg/7Y8GXAa"><FaDiscord/></a>
+                {/*  GitHub Repo  */}
+                <a href="https://github.com/taulazer/tau"><FaGithub/></a>
+                {/*  Twitter */}
+                <a href="https://twitter.com/___Laica"><FaTwitter/></a>
+                {/*  Donate (To be added)  */}
+            </div>
         </footer>
     </div>
   );
